9eae6535db98decf93f4028b07cabb738d099c49
Ebba Magnusson <ebbasofie@live.com>
2024-09-04 13:53:40 +0200

    discussion

--- DAT101-main/module_2/task_2-2/task_2-2.mjs
+++ DAT101-main/module_2/task_2-2/task_2-2.mjs
@@ -4,12 +4,20 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let result = 2 + 3 * 2 - 4 * 6;
+let withresult = (2 + 3) * (2 - 4) * 6;
+printOut(result);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let meter = 25;
+let centimeter = 34;
+const america = 25.4;
+let m_inches = meter * 1000;
+let c_inches = centimeter * 10;
+let inches = (m_inches * america) + (c_inches * america);
+printOut(inches);
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
