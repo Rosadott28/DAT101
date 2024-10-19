2cd18440cb3a6d14ca18efc57fb19a61710e1b2b
Ebba Magnusson <ebbasofie@live.com>
2024-10-19 20:49:57 +0200

    19/10 done3.2

--- DAT101-main/module_3/task_3-3/task_3-3.mjs
+++ DAT101-main/module_3/task_3-3/task_3-3.mjs
@@ -18,10 +18,35 @@ function printTodayInNorwegian() {
 printTodayInNorwegian();
 printOut(newLine);
 
-
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+// Funksjon som beregner antall dager igjen til lanseringen av 2XKO
+function calculateDaysUntilRelease() {
+    const releaseDate = new Date(2025, 4, 14); // Merk: Måneder er null-indeksert, så 4 er mai
+    const today = new Date();
+    // Beregner forskjellen i millisekunder
+    const difference = releaseDate - today;
+    // Konverterer millisekunder til dager
+    const daysRemaining = Math.ceil(difference / (1000 * 60 * 60 * 24));
+    // Skriver ut hvor mange dager som gjenstår ved å bruke .toString()
+    printOut("Det er " + daysRemaining.toString() + " dager igjen til lanseringen av 2XKO!! 🎮🔥");
+    // Returnerer antall dager som gjenstår
+    return daysRemaining;
+}
+
+// Funksjon som kombinerer dagens dato og nedtelling til lanseringen
+function displayDateAndCountdown() {
+    // Kaller funksjonen for å vise dagens dato
+    printOut("Dagens dato er: ");
+    printTodayInNorwegian();
+    // Kaller funksjonen for å vise nedtelling til lanseringen av 2XKO
+    calculateDaysUntilRelease();
+    printOut(newLine);
+}
+
+// Kaller funksjonen for å vise resultatene
+displayDateAndCountdown();
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
